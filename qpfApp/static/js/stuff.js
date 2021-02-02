ba9520b26dae7edb2a4e56baf1b52bab9d341ce8
onagbonoga <55672475+onagbonoga@users.noreply.github.com>
2021-02-01 23:09:25 -0600

    more changes, i cant remember what I did

--- qpfApp/static/js/stuff.js
+++ qpfApp/static/js/stuff.js
@@ -1,23 +1,28 @@
-/*let canvas = document.getElementById('canvas')
-let img = document.getElementById('img')
 
-img.onload = function(){
-
-	let x = -(img.width - canvas.offsetWidth)/2+"px"
-	let y = -(img.height - canvas.offsetHeight)/2 + "px"
-	console.log(x, "$$$$");
-	console.log(y, "$$$$");
-	let ratio = canvas.offsetWidth/img.width
-	console.log(ratio, "$$$$");
-
-	img.style.transform = `translate(${x}, ${y}) scale(${ratio})`
-}*/
+var x = 2 //x is the value the width and height of the image will be divided by
 function drawImage(){
+	
 	var imageContainer = document.getElementById("imageContainer");
 	imageContainer.style.display = "none"
-
 	var c = document.getElementById("myCanvas");
   	var ctx = c.getContext("2d");
   	var img = document.getElementById("myImage");
-  	ctx.drawImage(img,5,5, img.width/2, img.height/2);
+  	ctx.drawImage(img,5,5, img.width/x, img.height/x);
+}
+
+function ZoomOut(){
+	//drawImage(5,5,2,2)
+	var canvas = document.getElementById("myCanvas");
+	const ctx = canvas.getContext('2d');
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	x = x + 0.3;
+	drawImage();
+}
+
+function ZoomIn(){
+	var canvas = document.getElementById("myCanvas");
+	const ctx = canvas.getContext('2d');
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	x = x - 0.3;
+	drawImage();
 }
